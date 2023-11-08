fb6db178573129a6e9b2701a15ed354dc7256088
Phil <philippe.teigne@gmail.com>
2023-11-07 20:37:23 -0500

    export function as object

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,4 +4,4 @@ const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, callback);
 };
 
-module.exports = fetchBreedDescription;
+module.exports = { fetchBreedDescription };
