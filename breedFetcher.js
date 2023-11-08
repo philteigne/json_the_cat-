23d0d527dc16a6ecc092396af6a3e06a875c5554
Phil <philippe.teigne@gmail.com>
2023-11-07 19:40:17 -0500

    add request command that takes in user input and returns description

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,12 @@
 const request = require('request');
 
+const input = process.argv[2];
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const dataPull = JSON.parse(data.body);
+    console.log(dataPull[0].description);
+  }
+});
