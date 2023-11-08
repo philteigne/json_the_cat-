7175e7069f0cda3b0af174efa3985447fca01e9f
Phil <philippe.teigne@gmail.com>
2023-11-07 20:20:20 -0500

    move info logging to callback in index.js, breedFetcher now only handles request function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,7 @@
 const request = require('request');
 
-const args = process.argv[2];
-
-const requestData = (input) => {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    const dataPull = JSON.parse(data.body);
-    if (dataPull.length === 0) {
-      console.log("That's not a cat. This is only for cats.");
-      return;
-    }
-    console.log(dataPull[0].description);
-  });
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, callback);
 };
 
-requestData(args);
+module.exports = fetchBreedDescription;
---
+++ index.js
@@ -0,0 +1,12 @@
+const fetchBreedDescription = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    const pullData = JSON.parse(desc.body);
+    console.log(pullData[0].description);
+  }
+});
