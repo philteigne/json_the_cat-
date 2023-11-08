3f8122a5b2e5d308a48c402b98f0f085aa79fa40
Phil <philippe.teigne@gmail.com>
2023-11-07 21:05:07 -0500

    add test for siberian input

---
+++ test/breedFetcherTest.js
@@ -0,0 +1,21 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('#breedFetcher', () => {
+  it('should return the correct description for "Siberian"', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // no error in this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      let returnData = JSON.parse(desc.body);
+      returnData = returnData[0].description;
+
+      assert.equal(expectedDesc, returnData.trim());
+
+      done();
+    });
+  });
+});
+
