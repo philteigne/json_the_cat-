cd8c1c6249b5b4bbc7c681d498c3855ad0641cc1
Phil <philippe.teigne@gmail.com>
2023-11-07 21:25:55 -0500

    add return statements after console.log

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const breedName = process.argv[2];
 fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
     console.log('Error fetch details:', error);
+    return error;
   } else {
     const pullData = JSON.parse(desc.body);
     console.log(pullData[0].description);
+    return pullData[0].description;
   }
 });
