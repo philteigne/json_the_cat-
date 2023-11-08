0297660cb5f188013c2756841f682551ba88eda6
Phil <philippe.teigne@gmail.com>
2023-11-07 19:50:10 -0500

    function logs errors for invalid URL and prints a message if the breed entered is invalid

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,20 @@
 const request = require('request');
 
-const input = process.argv[2];
+const args = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
+const requestData = (input) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
     const dataPull = JSON.parse(data.body);
+    if (dataPull.length === 0) {
+      console.log("That's not a cat. This is only for cats.");
+      return;
+    }
     console.log(dataPull[0].description);
-  }
-});
+  });
+};
+
+requestData(args);
